937ecf5a69a2493fde69118e7afe34287db4a237
Yader Coca <ylcoca@gmail.com>
2021-07-18 00:39:50 -0300

    Asset Data types

--- Hahn.ApplicatonProcess.Application/Hahn.ApplicatonProcess.July2021.Domain/Model/Asset.cs
+++ Hahn.ApplicatonProcess.Application/Hahn.ApplicatonProcess.July2021.Domain/Model/Asset.cs
@@ -8,17 +8,18 @@ namespace Hahn.ApplicatonProcess.July2021.Domain
     public class Asset
     {
         [Key]
+        public string assetId { get; set; }
         public string Id { get; set; }
-        public string Rank { get; set; }
+        public int Rank { get; set; }
         public string Symbol { get; set; }
         public string Name { get; set; }
-        public string Supply { get; set; }
-        public string MaxSupply { get; set; }
-        public string MarketCapUsd { get; set; }
-        public string VolumeUsd24Hr { get; set; }
-        public string PriceUsd { get; set; }
-        public string ChangePercent24Hr { get; set; }
-        public string Vwap24Hr { get; set; }
+        public double Supply { get; set; }
+        public double? MaxSupply { get; set; }
+        public double MarketCapUsd { get; set; }
+        public double VolumeUsd24Hr { get; set; }
+        public double PriceUsd { get; set; }
+        public double ChangePercent24Hr { get; set; }
+        public double? Vwap24Hr { get; set; }
         public string Explorer { get; set; }
         public User User { get; set; }
     }
